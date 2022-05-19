4de7d5a0106be5b1e4f259925b7c897c164951b8
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-05-19 15:42:24 -0500

    Add image

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# chilipublish
+![CHILI_publish_Logo](https://user-images.githubusercontent.com/55994508/169401001-2f03fc67-7458-4c09-b189-329c9ee1df2f.jpg)
---
+++ script.js
@@ -0,0 +1,21 @@
+// Chilipublish code challenge
+
+let index = 4;
+
+// Your function returns a number but you're not saving it
+// index = changeIndex(index);
+// index = changeIndex(index);
+// changeIndex(index);
+// changeIndex(index);
+
+// if (index > 5) {
+//     console.log('Index bigger than 5');
+// } else {
+//     console.log('Index less than or equal to 5');
+// }
+
+// function changeIndex(index) {
+//     return index + 1;
+// }
+
+// output should be "Index bigger than 5"
